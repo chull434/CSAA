39e346db630a24ade6d1b6c468b253706aff520f
Christopher Hull <chull02@qub.ac.uk>
2018-10-22 20:48:23 +0100

    Registration Client And Server
    -Added Request
    -Added AccountRequest
    -Added User Model

---
+++ CSAA/Client/Requests/Request.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Client.Requests
+{
+    public class Request
+    {
+        protected HttpClient client;
+
+        public Request()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:62676/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
