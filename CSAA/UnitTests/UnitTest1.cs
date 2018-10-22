9605ad18f94d014a77254216c22a93e629b5da51
Christopher Hull <chull02@qub.ac.uk>
2018-10-22 14:21:31 +0100

    Created Solution
    -Added Server project
    -Added Client project
    -Added Unit tests project
    -Added Functional tests project
    -Added Models Project

---
+++ CSAA/UnitTests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Machine.Specifications;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    public class Context
+    {
+        Establish context = () =>
+        {
+            
+        };
+    }
+
+    #region Constructor Tests
+
+    public class when_I_Construct : Context
+    {
+        static bool test;
+
+        Establish context = () =>
+        {
+            test = false;
+        };
+
+        Because of = () => 
+            test = true;
+
+        It Constructs = () => test.ShouldBeTrue();
+    }
+
+    #endregion
+}
