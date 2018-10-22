1687c6293bd98883d97f037eb51ccc266563c77b
Christopher Hull <chull02@qub.ac.uk>
2018-10-22 15:33:40 +0100

    -Added EF migrations -Added repository pattern

---
+++ CSAA/Server/App_Data/ServerDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using Server.Models;
+
+namespace Server.App_Data
+{
+    public class ServerDbContext : ApplicationDbContext
+    {
+        public ServerDbContext() : base()
+        {
+
+        }
+    }
+}
