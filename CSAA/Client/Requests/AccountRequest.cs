14ce765cb42c921a19eafed8da2870d43cc11bf0
rmcclelland06 <rmcclelland06@qub.ac.uk>
2018-10-23 14:26:27 +0100

    Login Request
    - Pair Programming with Keith Harris
    - Login request added.

--- CSAA/Client/Requests/AccountRequest.cs
+++ CSAA/Client/Requests/AccountRequest.cs
@@ -21,5 +21,35 @@ namespace Client.Requests
 
             return response.IsSuccessStatusCode;
         }
+
+        public bool Login(string email, string password)
+        {
+            return LoginAsync(email, password).GetAwaiter().GetResult();
+        }
+
+        public async Task<bool> LoginAsync(string email, string password)
+        {
+            var loginData = new Dictionary<string, string>();
+            loginData.Add("grant_type", "password");
+            loginData.Add("username", email);
+            loginData.Add("password", password);
+
+            var response = await client.PostAsync("/token", new FormUrlEncodedContent(loginData)).ConfigureAwait(false);
+
+            var message = await response.Content.ReadAsAsync<LoginData>().ConfigureAwait(false);
+
+            response.EnsureSuccessStatusCode();
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", message.access_token);
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+
+    public class LoginData
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public string userName { get; set; }
     }
 }
