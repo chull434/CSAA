3cb452a4d490f45aff1f333c396a454913e0f7b2
Michael Dyer <mdyer01@qub.ac.uk>
2018-10-24 16:57:54 +0100

    Refactoring with chris's suggested changes

--- CSAA/Client/Views/Registration.xaml.cs
+++ CSAA/Client/Views/Registration.xaml.cs
@@ -27,71 +27,91 @@ namespace Client.Views
             InitializeComponent();
         }
 
-        private bool CheckEmail(String email)
-        {
-            return new EmailAddressAttribute().IsValid(email);
-        }
-
         private void btn_Register_Click(object sender, RoutedEventArgs e)
         {
             lbl_InvalidPassword.Visibility = Visibility.Hidden;
             lbl_InvalidFields.Visibility = Visibility.Hidden;
-            String firstName = txt_FirstName.Text;
-            String secondName = txt_Surname.Text;
-            bool isEmptyFields = !(string.IsNullOrEmpty(txt_FirstName.Text.ToString())) && !(string.IsNullOrEmpty(txt_Surname.Text.ToString())) && !(string.IsNullOrEmpty(txt_Email.Text.ToString()));
-            bool isPasswordValid = !(string.IsNullOrEmpty(pwb_Input.Password)) && !(string.IsNullOrEmpty(pwb_Input_Confirm.Password)) && pwb_Input.Password == pwb_Input_Confirm.Password;
 
+            var ModelStateValid = true;
 
-            if (isEmptyFields)
+            ModelStateValid = EmptyFieldValidation();
+
+            if(ModelStateValid)
             {
-
-
-                if (CheckEmail(txt_Email.Text))
-                {
-
-                    if (isPasswordValid)
-                    {
-                        
-                        var AccountRequest = new AccountRequest();
-                        var user = new User
-                        {
-                            Name = firstName + " " + secondName,
-                            Email = txt_Email.Text,
-                            Password = pwb_Input.Password,
-                            product_owner = chk_ProductOwner.IsChecked.Value,
-                            scrum_master = chk_ScumMaster.IsChecked.Value,
-                            developer = chk_Developer.IsChecked.Value
-                        };
-                        AccountRequest.Register(user);
-                    }
-                    else
-                    {
-                        lbl_InvalidPassword.Visibility = Visibility.Visible;
-                        if (!(isPasswordValid))
-                        {
-                            lbl_InvalidPassword.Visibility = Visibility.Visible;
-                        }
-                    }
-                }
-                else
-                {
-                    lbl_InvalidFields.Content = "Invalid Email";
-                    lbl_InvalidFields.Visibility = Visibility.Visible;
-                    if (!(isPasswordValid))
-                    {
-                        lbl_InvalidPassword.Visibility = Visibility.Visible;
-                    }
-                }
+                ModelStateValid = EmailValidation();
             }
-            else
+
+            ModelStateValid = PasswordValidation();
+
+            if (ModelStateValid)
+            {
+                Register();
+            }
+        }
+
+        private void Register()
+        {
+            var AccountRequest = new AccountRequest();
+            var user = new User
+            {
+                Name = txt_FirstName.Text + " " + txt_Surname.Text,
+                Email = txt_Email.Text,
+                Password = pwb_Input.Password,
+                product_owner = chk_ProductOwner.IsChecked.Value,
+                scrum_master = chk_ScumMaster.IsChecked.Value,
+                developer = chk_Developer.IsChecked.Value
+            };
+            AccountRequest.Register(user);
+        }
+
+        private bool PasswordValidation()
+        {
+            bool isPasswordInvalid = string.IsNullOrEmpty(pwb_Input.Password) || 
+                                     string.IsNullOrEmpty(pwb_Input_Confirm.Password) || 
+                                     pwb_Input.Password != pwb_Input_Confirm.Password;
+
+            if (isPasswordInvalid)
+            {
+                lbl_InvalidPassword.Visibility = Visibility.Visible;
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool EmailValidation()
+        {
+            if (!CheckEmailIsValid(txt_Email.Text))
+            {
+                lbl_InvalidFields.Content = "Invalid Email";
+                lbl_InvalidFields.Visibility = Visibility.Visible;
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool EmptyFieldValidation()
+        {
+            bool areFieldsEmpty = string.IsNullOrEmpty(txt_FirstName.Text) || 
+                                  string.IsNullOrEmpty(txt_Surname.Text) || 
+                                  string.IsNullOrEmpty(txt_Email.Text);
+
+            if (areFieldsEmpty)
             {
                 lbl_InvalidFields.Content = "Please Populate all fields";
                 lbl_InvalidFields.Visibility = Visibility.Visible;
-                if (!(isPasswordValid))
-                {
-                    lbl_InvalidPassword.Visibility = Visibility.Visible;
-                }
+
+                return false;
             }
+
+            return true;
+        }
+
+        private bool CheckEmailIsValid(string email)
+        {
+            return new EmailAddressAttribute().IsValid(email);
         }
     }
 }
