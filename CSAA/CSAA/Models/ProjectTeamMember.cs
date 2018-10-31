8d078745300c0180510a2ca3c6a8c659703a28fe
Christopher Hull <chull02@qub.ac.uk>
2018-10-31 23:57:55 +0000

    Fixing create project button and project team member data model PK and FK

--- CSAA/CSAA/Models/ProjectTeamMember.cs
+++ CSAA/CSAA/Models/ProjectTeamMember.cs
@@ -6,12 +6,12 @@ namespace CSAA.Models
 {
     public class ProjectTeamMember
     {
-        [Key]
-        [Column(Order = 1)]
+        [Key, Column(Order = 0)]
         public string UserId { get; set; }
 
-        [Key]
-        [Column(Order = 2)]
+        [Key, Column(Order = 1),ForeignKey("Project")]
+        public Guid ProjectId { get; set; }
+
         public Project Project { get; set; }
 
         public Role Role { get; set; }
