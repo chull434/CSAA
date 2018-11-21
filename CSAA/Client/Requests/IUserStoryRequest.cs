af1e6f58dc1405ed66cf5c672c30598bf696cb78
Johnston Richard York <jyork01@qub.ac.uk>
2018-11-21 16:07:00 +0000

    added client requests for UserStory - JY & MD

---
+++ CSAA/Client/Requests/IUserStoryRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using CSAA.ServiceModels;
+
+namespace Client.Requests
+{
+    interface IUserStoryRequest
+    {
+        List<UserStory> GetUserStories();
+        UserStory GetUserStoryById(string userStoryId);
+        string CreateUserStory(UserStory userStory);
+    }
+}
---
+++ CSAA/Client/Requests/UserStoryRequest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CSAA.ServiceModels;
+
+namespace Client.Requests
+{
+    public class UserStoryRequest : Request, IUserStoryRequest
+    {
+        #region Constructor
+
+        public UserStoryRequest(IHttpClient client) : base(client)
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public List<UserStory> GetUserStories()
+        {
+            return GetUserStoriesAsync().GetAwaiter().GetResult();
+        }
+
+        public UserStory GetUserStoryById(string userStoryId)
+        {
+            return GetUserStoryByIdAsync(userStoryId).GetAwaiter().GetResult();
+        }
+
+        public string CreateUserStory(UserStory userStory)
+        {
+            return CreateUserStoryAsync(userStory).GetAwaiter().GetResult();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private async Task<List<UserStory>> GetUserStoriesAsync()
+        {
+            var response = await client.GetAsync("api/UserStory").ConfigureAwait(false);
+            var result = await CheckResponse(response).ConfigureAwait(false);
+            return await response.Content.ReadAsAsync<List<UserStory>>().ConfigureAwait(false);
+        }
+
+        private async Task<UserStory> GetUserStoryByIdAsync(string userStoryId)
+        {
+            var response = await client.GetAsync("api/UserStory/" + userStoryId).ConfigureAwait(false);
+            var result = await CheckResponse(response).ConfigureAwait(false);
+            return await response.Content.ReadAsAsync<UserStory>().ConfigureAwait(false);
+        }
+
+        private async Task<string> CreateUserStoryAsync(UserStory userStory)
+        {
+            var response = await client.PostAsJsonAsync("api/UserStory", userStory).ConfigureAwait(false);
+            var result = await CheckResponse(response).ConfigureAwait(false);
+            var userStoryId = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return userStoryId.Substring(1, userStoryId.Length - 2);
+        }
+
+        #endregion
+    }
+}
